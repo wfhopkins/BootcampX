a67d11bc4b117c8813e30ab67255017beb80777b
wfhopkins <will.hop@gmail.com>
2023-06-29 13:42:19 -0700

    sanitize students and teachers inputs

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,18 +7,23 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const cohortName = process.argv[2];
+const values = [cohortName];
+
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`) 
+JOIN assistance_requests ON assistance_requests.teacher_id = teachers.id
+JOIN students ON assistance_requests.student_id = students.id
+JOIN cohorts ON students.cohort_id = cohorts.id
+WHERE cohorts.name = $1
+ORDER BY teacher;`;
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
   })
+  process.exit(1);
 })
 .catch(err => console.error('query error', err.stack));
